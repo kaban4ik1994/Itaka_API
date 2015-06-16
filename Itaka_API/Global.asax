02cf508a83adf471a748485e01a83b4437c41058
Andrey <kaban4ik19901990@mail.ru>
2015-06-16 16:42:45 +0300

    First commit

---
+++ Itaka_API/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Itaka_API.WebApiApplication" Language="C#" %>
---
+++ Itaka_API/Models/ItakaParameters.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Itaka_API.Models
+{
+    /*
+     * filter = typeof filter !== 'undefined' ? filter : '';
+	currpage = typeof currpage !== 'undefined' ? currpage : 1;
+	pricetype = typeof pricetype !== 'undefined' ? pricetype : 'person';
+	eventtype = typeof eventtype !== 'undefined' ? eventtype : 0;
+	date_from = typeof date_from !== 'undefined' ? date_from : '';
+	date_to = typeof date_to !== 'undefined' ? date_to : '';
+	duration = typeof duration !== 'undefined' ? duration : '';
+	adults = typeof adults !== 'undefined' ? adults : 2;
+	childs = typeof childs !== 'undefined' ? childs : 0;
+	child_age = typeof child_age !== 'undefined' ? child_age : ["12.06.2010"];
+	standard = typeof standard !== 'undefined' ? standard : '';
+	price = typeof price !== 'undefined' ? price : '';
+	promotions = typeof promotions !== 'undefined' ? promotions : ["itakihit"];
+	grade = typeof grade !== 'undefined' ? grade : '';
+	order = typeof order !== 'undefined' ? order : "recommended|asc";
+     */
+
+    public class ItakaParameters
+    {
+        public string Filter { get; set; }
+
+        public string CurrPage { get; set; }
+
+        public string PriceType { get; set; }
+
+        public string EventType { get; set; }
+
+        public string Date_From { get; set; }
+
+        public string Date_To { get; set; }
+
+        public string Duration { get; set; }
+
+        public string Adults { get; set; }
+
+        public string Childs { get; set; }
+
+        public string Child_Age { get; set; }
+
+        public string Standard { get; set; }
+
+        public string Price { get; set; }
+
+        public string Promotions { get; set; }
+
+        public string Grade { get; set; }
+
+        public string Order { get; set; }
+    }
+}
